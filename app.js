c807f5631cd866709f702afaebfe05b502bff108
Kavija Karunarathna <kavisuper@gmail.com>
2020-07-04 21:59:29 +0530

    Created repo with half-completed project

---
+++ app.js
@@ -0,0 +1,95 @@
+var express = require("express"),
+	app = express(),
+ 	bodyParser = require("body-parser"),
+	mongoose = require("mongoose"),
+	campground = require("./models/campground"),
+	seedDB = require("./seeds"),
+	Comment = require("./models/comments")
+   
+mongoose.connect("mongodb://localhost:27017/yelp_camp", {useNewUrlParser: true});
+app.use(bodyParser.urlencoded({extended: true}));
+app.set("view engine", "ejs");
+seedDB();
+
+
+
+app.get("/", function(req, res){
+	res.render("landing");
+})
+
+app.get("/campgrounds", function(req, res){	
+	campground.find({}, function(err, allCampgrounds){
+		if (err){
+			console.log(err);
+		} else {
+			res.render("campgrounds/index", {campgrounds: allCampgrounds})
+		}
+	});
+})
+
+app.get("/campgrounds/new", function(req, res){
+	res.render("campgrounds/new");
+})
+
+app.post("/campgrounds", function(req, res){	
+	campground.create(
+	{
+		name: req.body.name,
+	 	image: req.body.image,
+	 	description: req.body.description 
+	},
+	function(err , campground){
+		if (err){
+			res.redirect("campgrounds/new");
+		}
+		else {
+			res.redirect("/campgrounds")
+		}
+	})	
+})
+
+app.get("/campgrounds/:id", function(req, res){
+	campground.findById(req.params.id).populate("comments").exec(function(err, foundCampground){
+		if (err){
+			console.log(err);
+		} else {
+			console.log(foundCampground.comments);
+			res.render("campgrounds/show", {campground: foundCampground});
+		}
+	})
+	
+})
+
+app.get("/campgrounds/:id/comments/new", function(req, res){
+	campground.findById(req.params.id, function(err, foundCampground) {
+		if (err){
+			console.log(err)
+		} else {
+			res.render("comments/new", {campground: foundCampground})
+		}
+	});
+})
+
+app.post("/campgrounds/:id/comments", function(req, res){
+	campground.findById(req.params.id, function(err, foundCampground) {
+		if (err){
+			console.log(err)
+		} else {
+			Comment.create(req.body.comment , function(err, comment){
+				if (err){
+					console.log(err);
+				}
+				else {
+					foundCampground.comments.push(comment);
+					foundCampground.save();
+					res.redirect("/campgrounds/"+foundCampground._id)
+				}
+			})
+		}
+	});
+});
+
+
+app.listen(3000, function(){
+	console.log("The YelpCamp Server has started")
+})
